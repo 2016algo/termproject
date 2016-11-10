d9c4918de24a095c9133e88f88309d2f62b3dfef
user <user@USER-PC>
2016-11-10 14:54:20 +0900

    인풋 테스트 추가

--- CafeManagement/CafeManagement/Main.cpp
+++ CafeManagement/CafeManagement/Main.cpp
@@ -2,9 +2,15 @@
 #include <string>
 #include <iostream>
 
-
+using namespace std;
 
 void main(void)
 {
+	ifstream inFile("output.txt");
 
+	while (!inFile.eof()) {
+		inFile.getline(inputString, 100);
+		cout << inputString << endl;
+	}
+	inFile.close();
 }
