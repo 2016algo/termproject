54429396d896ec1c19ea3720fe34c95b0a90a6ce
Sunpark <reverse199@gmail.com>
2016-11-24 13:11:15 +0900

    주석 작업 완료

--- CafeManagement/CafeManagement/Baristar.h
+++ CafeManagement/CafeManagement/Baristar.h
@@ -5,12 +5,23 @@
 using namespace std;
 
 /*
-Struct order 
+Class order 
 => 입력받은 주문
 
-orderTime : 주문 시간
-coffeCnt : 주문 받은 커피 타입에 해당하는 커피 개수
-coffeType : 주문받은 커피 타입
+Queue에 주문을 넣을 때 사용한다.
+
+	* Variable *
+	orderTime => 주문을 받은 시간
+	makingTime => 주문 받은 커피를 만드는데 드는 시간
+	coffeCnt => 주문 받은 커피 타입에 해당하는 커피 개수
+	coffeType => 주문 받은 커피 타입
+
+	* Constructor *
+	Order() => Basic Consturctor
+	Order(int orderTime, int coffeCnt, string coffeType) => 주문 시간, 커피 수, 커피 타입을 설정
+
+	* Method *
+	void setMakingTime() => 커피 타입에 따른 소요 시간 설정
 */
 typedef class Order {
 public:
@@ -31,37 +42,28 @@ public:
 	}
 
 	void setMakingTime() {
-		if (coffeeType == "Espresso") {
+		if (coffeeType == "Espresso")
 			makingTime = 1;
-		}
-		else if (coffeeType == "Americano") {
+		else if (coffeeType == "Americano")
 			makingTime = 2;
-		}
-		else if (coffeeType == "CafeLatte") {
+		else if (coffeeType == "CafeLatte")
 			makingTime = 4;
-		}
-		else if (coffeeType == "CafeMocha") {
+		else if (coffeeType == "CafeMocha") 
 			makingTime = 5;
-		}
-		else if (coffeeType == "VanillaLatte") {
+		else if (coffeeType == "VanillaLatte") 
 			makingTime = 4;
-		}
-		else if (coffeeType == "MochaLatte") {
+		else if (coffeeType == "MochaLatte")
 			makingTime = 5;
-		}
-		else if (coffeeType == "Macchiato") {
+		else if (coffeeType == "Macchiato")
 			makingTime = 5;
-		}
-		else if (coffeeType == "Frappucino") {
+		else if (coffeeType == "Frappucino")
 			makingTime = 10;
-		}
-		else if (coffeeType == "Smoothie") {
+		else if (coffeeType == "Smoothie")
 			makingTime = 8;
-		}
-		else if (coffeeType == "HerbalTea") {
+		else if (coffeeType == "HerbalTea")
 			makingTime = 3;
-		}
 	}
+
 }order;
 
 
@@ -70,8 +72,24 @@ Class baristar
 => 일을 분배받을 바리스타 클래스
 
 Queue에서 주문이 pop되어 분배를 시작하면
-각 바리스타가 일을 했던 시간을 기준으로
+각 바리스타가 일을 했던 시간, 현재 일을 해야하는 시간, 주문 받은 횟수를 기준으로
 제일 일을 덜 한 바리스타가 주문을 먼저 받게 된다.
+
+	* Variable *
+	int completeOrderTime => 바리스타가 일을 수행했던 시간
+	int curOrderTime => 바리스타가 배분받은 일을 해야하는 시간
+	int orderCnt => 바리스타가 만들 커피 수
+	queue<order> executeList => 바리스타가 배분 받은 일을 저장하는 queue
+	vector<order> outputList => 바리스타가 일을 했던 것을 기록하기 위한 vector
+
+	* Constructor *
+	Baristar() => Basic Constructor
+
+	* Operator overloading *
+	bool operator<(const Baristar &u) const => 일을 제일 덜 한 바리스타를 찾기 위한 operator overloading
+	
+	* Method *
+	void takeOrder(order o) => 배분받은 order를 executeList와 outputList에 push하고 curOrderTime과 orderCnt를 증가시킨다.
 */
 typedef class Baristar {
 public:
@@ -99,16 +117,10 @@ public:
 
 	//method
 	void takeOrder(order o) {
-		//printf("테이크오더 실행 됨\n");
 		executeList.push(o);
-		//printf("실행 후 queue size %d\n", executeList.size());
 		outputList.push_back(o);
 		curOrderTime += o.makingTime;
 		orderCnt++;
-	}
-
-	void checkOrder(int wholeTime) {
-		int executeTime = wholeTime - executeList.front().orderTime;
 	}
 }baristar;
 
--- CafeManagement/CafeManagement/Main.cpp
+++ CafeManagement/CafeManagement/Main.cpp
@@ -1,3 +1,25 @@
+/*
+	Algorithm Term Project
+	20113274 김한결
+	20113281 박순광
+	20113300 윤명식
+	20113309 이   식
+*/
+
+/*
+Cafe Menu
+1. Espresso
+2. Americano
+3. CafeLatte
+4. CafeMocha
+5. VanillaLatte
+6. MochaLatte
+7. Macchiato
+8. Frappucino
+9. Smoothie
+10. HerbalTea
+*/
+
 #include <cstdio>
 #include <string>
 #include <iostream>
@@ -6,88 +28,79 @@
 
 using namespace std;
 
-int m;
+/*
+	* Variable *
+	int n => 입력받을 주문의 수
+	int m => 입력받을 바리스타의 수
+	int wholeTime => 분 단위, 일의 시간흐름을 위한 변수, 각 바리스타가 주문 Queue에서 각자 일을 배분받아서 모두 수행을 완료하는 시간
+	queue<order> orderList => order 구조체로 이루어진 queue, 입력받은 주문들을 저장한다.
+	vector<baristar> baristarList => baristar 구조체로 이루어진 vector, 입력받은 바리스타의 수 만큼의 크기로 구성한다.
+
+	* Method *
+	void orderinit()
+	=> 주문을 orderList에 저장
+	input.txt에서 주문들을 입력 받은 후 orderList에 한 잔 기준으로 저장
+
+	첫 줄에는
+	n : 주문 총 량, m : 바리스타의 수 가 입력되며
+	그 다음 줄부터 n-1개의 줄에는
+	k : 주문된 커피 종류 수, orderTime : 주문 시간과 이후 k개의
+	coffeeType : 커피 종류, coffeeCnt : 커피 수가 입력된다.
+
+	bool checkExit();
+	=>  주문이 모두 수행되었는지 체크
+	각 바리스타들을 순회하며
+	orderList가 비어있지 않거나 
+	바리스타의 executeList가 비어있지 않거나 
+	바리스타의 curOrderTime이 0이 아니면 일이 끝나지 않았으므로 true 반환
+
+	void executeOrder()
+	=> 각 바리스타가 분배받은 일을 1분 만큼 수행한다.
+	만일 바리스타가 executeList의 front에 있는 일을 수행하고 나서 커피의 makingTime이 0이 되면 커피가 만들어진 것이므로 pop한다.
+	
+	distributeOrder()
+	=> 일을 수행한 후 일을 제일 적게 한, 그리고 적게 할 바리스타를 찾아서 orderList에 있는 주문을 배분한다.
+	일을 제일 적게 한 바리스타는 baristarList를 sorting 하면 맨 처음으로 오게 되어있다.
+
+*/
+int n, m; 
 int wholeTime = 0;
 queue<order> orderList;
 vector<baristar> baristarList;
 
 void orderInit();
 bool checkExit();
+void executeOrder();
+void distributeOrder();
 
 int main(){
+
 	orderInit();
 
-
-	int tmpcnt = 1;
-	queue<order> q = orderList;
-	while (!q.empty()) {
-		auto tmp = q.front();
-		cout << tmpcnt++ << " 번 째 주문\n";
-		cout << "주문 한 시간 : " << tmp.orderTime << " \n주문한 커피 종류 : " << tmp.coffeeType << "\n해당 커피 만드는 시간" << tmp.makingTime <<"\n";
-		q.pop();
-	}
-
 	while (checkExit()) {
-		//바리스타 일 진행시키기
-
-		//printf("어디서?\n");
-		for (int i = 0; i < m; i++) {
-			if (baristarList[i].curOrderTime != 0) {
-				if (baristarList[i].executeList.front().makingTime != 0) {
-					baristarList[i].executeList.front().makingTime-=1;
-					baristarList[i].completeOrderTime+=1;
-					baristarList[i].curOrderTime-=1;
-				}
-				if (baristarList[i].executeList.front().makingTime == 0) {
-					baristarList[i].executeList.pop();
-				}
-			}
-		}
-
-		//일 분배
-		while (!orderList.empty() && wholeTime == orderList.front().orderTime) {
-			auto tmp = orderList.front();
-
-			//일 제일 적게한 바리스타 찾기
-			sort(baristarList.begin(), baristarList.end());
-
-			baristarList[0].takeOrder(tmp);
-			orderList.pop();
-		}
-
+		executeOrder();
+		distributeOrder();
 		wholeTime++;
 	}
 
 	//일 제일 적게한 바리스타 찾기
-	sort(baristarList.begin(), baristarList.end());
-	for (int i = 0; i < m; i++) {
-		cout << i + 1 << " 번 째 바리스타\n";
-		cout << "총 일 한 시간 : " << baristarList[i].completeOrderTime;
-		cout << "\n만든 커피 개수 : " << baristarList[i].orderCnt;
-		cout << "\n";
-		int bSize = baristarList[i].outputList.size();
-		for (int j = 0; j < bSize; j++) {
-			cout << baristarList[i].outputList[j].orderTime << "\n";
-			cout << baristarList[i].outputList[j].coffeeType << "\n";
-			cout << baristarList[i].outputList[j].makingTime << "\n";
-			cout << "\n";
-		}
-		cout << "\n";
-	}
-	printf("%d\n");
+	//sort(baristarList.begin(), baristarList.end());
+	//for (int i = 0; i < m; i++) {
+	//	cout << i + 1 << " 번 째 바리스타\n";
+	//	cout << "총 일 한 시간 : " << baristarList[i].completeOrderTime;
+	//	cout << "\n만든 커피 개수 : " << baristarList[i].orderCnt;
+	//	cout << "\n";
+	//	int bSize = baristarList[i].outputList.size();
+	//	for (int j = 0; j < bSize; j++) {
+	//		cout << baristarList[i].outputList[j].orderTime << "\n";
+	//		cout << baristarList[i].outputList[j].coffeeType << "\n";
+	//		cout << baristarList[i].outputList[j].makingTime << "\n";
+	//		cout << "\n";
+	//	}
+	//	cout << "\n";
+	//};
 	return 0;
 }
-
-/*
-void orderInit()
-input.txt에서 주문들을 입력 받은 후 orderList에 한 잔 기준으로 저장
-
-첫 줄에는 
-n : 주문 총 량, m : 바리스타의 수 가 입력되며 
-그 다음 줄부터 n-1개의 줄에는
-k : 주문된 커피 종류 수, orderTime : 주문 시간과 이후 k개의
-coffeeType : 커피 종류, coffeeCnt : 커피 수가 입력된다.
-*/
 
 void orderInit() {
 	ifstream inFile;
@@ -95,7 +108,6 @@ void orderInit() {
 	if (inFile.fail())
 		exit(1);
 
-	int n; //총 주문 량, 바리스타 수
 	inFile >> n >> m;
 	baristarList.resize(m); //바리스타 init
 	
@@ -126,3 +138,29 @@ bool checkExit() {
 	return false;
 }
 
+void executeOrder() {
+	//바리스타 일 진행시키기
+	for (int i = 0; i < m; i++) {
+		if (baristarList[i].curOrderTime != 0) {
+			if (baristarList[i].executeList.front().makingTime != 0) {
+				baristarList[i].executeList.front().makingTime -= 1;
+				baristarList[i].completeOrderTime += 1;
+				baristarList[i].curOrderTime -= 1;
+			}
+			if (baristarList[i].executeList.front().makingTime == 0) {
+				baristarList[i].executeList.pop();
+			}
+		}
+	}
+}
+
+void distributeOrder() {
+	//일 분배
+	while (!orderList.empty() && wholeTime == orderList.front().orderTime) {
+		auto tmp = orderList.front();
+		//일 제일 적게한 바리스타 찾기
+		sort(baristarList.begin(), baristarList.end());
+		baristarList[0].takeOrder(tmp);
+		orderList.pop();
+	}
+}
